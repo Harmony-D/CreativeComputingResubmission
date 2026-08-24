e775f73bef7c7f9713ec130b7b219282265a7707
Harmony-D <162699330+Harmony-D@users.noreply.github.com>
2026-08-24 19:18:17 +0100

    Update and rename Animation.ipynb to Animation.py

---
+++ Animation.py
@@ -0,0 +1,5 @@
+''' Importing Libraries '''
+# https://www.geeksforgeeks.org/python/using-matplotlib-for-animations/
+import matplotlib.animation as animation
+import matplotlib.pyplot as plt
+import numpy as np
